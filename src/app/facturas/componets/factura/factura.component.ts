8e0d2b9634a37233214fa54924f46e5c709fb541
Tony Abril <antabrrue@alu.edu.gva.es>
2025-01-06 20:41:53 +0100

    factura funcional

--- src/app/facturas/componets/factura/factura.component.ts
+++ src/app/facturas/componets/factura/factura.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Factura } from '../../factura';
 
 @Component({
   selector: 'app-facturas-factura',
+  standalone: false,
   templateUrl: './factura.component.html',
   styleUrl: './factura.component.css'
 })
-export class FacturaComponent {
 
+export class FacturaComponent {
+  //Array de facturas
+  public facturas: Factura[] = [
+    new Factura(1, '01/01/2024', true, 25),
+    new Factura(2, '01/01/2024', true, 52),
+    new Factura(3, '01/01/2024', true, 19),
+    new Factura(4, '01/01/2024', true, 22)
+  ];
+
+  //Puntero para seleccionar en el array
+  public seleccionado: number = 0;
+
+  //Actualiza su lista con los cambios que le pase la clase hija (lista)
+  actualizarFacturas(facturasActualizadas: Factura[]) {
+    this.facturas = facturasActualizadas;
+    if (this.seleccionado > this.facturas.length - 1) 
+      this.seleccionado = 0;
+  }
+
+  //Crea una nueva factura con valores por defecto y le pone el puntero
+  public nuevaFactura() {
+    this.facturas.push(new Factura(this.facturas.length + 1, '01/01/2024', true, 0));
+    this.seleccionado = this.facturas.length - 1;
+  }
 }
--- src/app/facturas/facturas.module.ts
+++ src/app/facturas/facturas.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { FacturaComponent } from './componets/factura/factura.component';
 import { ListaComponent } from './componets/lista/lista.component';
-
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ListaComponent } from './componets/lista/lista.component';
     ListaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule   
   ],
   exports: [
     FacturaComponent,
